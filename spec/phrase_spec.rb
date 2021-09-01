df06286e91134dd2927d73d8aed51a49af8b15ca
Andrew Smith <c13andrew.smith@gmail.com>
2021-08-31 18:49:45 -0700

    test: #parse_guess

--- spec/phrase_spec.rb
+++ spec/phrase_spec.rb
@@ -11,13 +11,25 @@ RSpec.describe Phrase do
     phrase = Phrase.new
 
     expect(phrase.secret_phrase.length).to eq(4)
+    expect(phrase.secret_phrase).to be_kind_of(Array)
     #expect(phrase.secret_phrase).to include(["r", "g", "b", "y"])
   end
 
   it 'has an empty guess_phrase array' do
     phrase = Phrase.new
     expect(phrase.guess_phrase.length).to eq(0)
+    expect(phrase.secret_phrase).to be_kind_of(Array)
     #expect(phrase.secret_phrase).to include("r" || "g" || "b" || "y")
+  end
+
+  describe '#parse_guess' do
+    it 'parses the guess into an array' do
+      phrase = Phrase.new
+      guess = "rgby"
+      phrase.parse_guess(guess)
+      expect(phrase.guess_phrase.length).to eq(4)
+      expect(phrase.guess_phrase).to be_kind_of(Array)
+    end
   end
 
 end
