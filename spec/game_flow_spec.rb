08a041b152fe05342ff9d2cc1cfd9f2b3f693e48
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-01 19:06:39 -0600

    Test: edit #initialize test

--- spec/game_flow_spec.rb
+++ spec/game_flow_spec.rb
@@ -6,9 +6,15 @@ describe GameFlow do
 
   describe "#initialize" do
     it "creates an instance of GameFlow with readable attributes" do
-      game = GameFlow.new
+      phrase = Phrase.new
+      guess = "rgby"
+      phrase.parse_guess(guess)
+      game = GameFlow.new(phrase)
 
+      expect(game.phrase).to be_an_instance_of(Phrase)
       expect(game).to be_an_instance_of(GameFlow)
+      expect(game.phrase.guess_phrase).to eq(['r', 'g', 'b', 'y'])
+      expect(game.phrase.secret_phrase.length).to eq(4)
     end
   end
 
