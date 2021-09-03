5bec8f216e566e542bacf8ba4411185116f5987f
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-03 08:13:52 -0600

    Create #go method

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -1,3 +1,21 @@
-class GameLoop 
+class GameLoop
+
+  attr_reader :game_repl
+
+  def initialize
+    @game_repl = game_repl
+  end
+
+  def go
+    again = 'yes'
+    until again == 'no'
+      @game_repl = GameRepl.new
+      @game_repl.play
+    end
+    p "You have won the game!"
+    p "Would you like to play again? Enter 'yes' or 'no'"
+    print "> "
+    again = $stdin.gets.chomp
+  end
 
 end
