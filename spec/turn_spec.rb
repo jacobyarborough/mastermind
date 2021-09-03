b69b8ccdaa245ded0f84896bd789e10a40fb3cf3
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-02 19:43:57 -0600

    Refactor game_won? test

--- spec/turn_spec.rb
+++ spec/turn_spec.rb
@@ -2,19 +2,19 @@ require 'rspec'
 require './lib/phrase'
 require './lib/turn'
 
-describe Turn do
+describe GameRepl do
 
   describe "#initialize" do
     it "initiazes an instance of Turn" do
-    turn = Turn.new
+    turn = GameRepl.new
 
-    expect(turn).to be_an_instance_of(Turn)
+    expect(turn).to be_an_instance_of(GameRepl)
     end
   end
 
   describe "#game_won?" do
     it "returns true if the secret phrase has been guessed" do
-      turn = Turn.new
+      turn = GameRepl.new
       sec_phrase = turn.phrase.secret_phrase
       turn.phrase.parse_guess(sec_phrase.join)
 
@@ -24,7 +24,7 @@ describe Turn do
 
   describe "#assign_guss" do
     it "assigns the user guess to the phrase object" do
-      turn = Turn.new
+      turn = GameRepl.new
       turn.assign_guess
 
       expect(turn.phrase.guess_phrase).to eq(['r', 'g', 'b', 'y'])
