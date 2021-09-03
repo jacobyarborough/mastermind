1c747b0d2aaeb89c251d0d93460f9cb6f3b28e83
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-02 20:20:11 -0600

    Refactor #initialize test

--- spec/game_loop_spec.rb
+++ spec/game_loop_spec.rb
@@ -1,15 +1,16 @@
 require 'rspec'
 require './lib/phrase'
 require './lib/game_repl'
+require './lib/game_loop'
 
 describe GameLoop do
 
   describe "#initialize" do
-    "it creates an instance of the GameLoop class with attributes" do
+    it "creates an instance of the GameLoop class with attributes" do
       game_loop_1 = GameLoop.new
 
       expect(game_loop_1).to be_an_instance_of(GameLoop)
     end
   end
 
-end 
+end
