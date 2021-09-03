0453164ca5edfeef3b6b0538de369ba19be812e4
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-03 09:53:59 -0600

    Refactor #go

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -7,15 +7,22 @@ class GameLoop
   end
 
   def go
-    again = 'yes'
-    until again == 'no'
-      @game_repl = GameRepl.new
-      @game_repl.play
+    game_status = ''
+    while game_status != 'e'
+      p 'Welcome to Mastermind. If you would like to start the game \nenter y'
+      game_status = $stdin.gets.chomp
+      if game_status == 'y'
+        @game_repl = GameRepl.new
+        @game_repl.play
+        p "You have won the game!"
+        #p "Would you like to play again? Enter 'yes' or 'no'"
+        #print "> "
+      elsif game_status == 'i'
+        p "here is some info"
+      elsif game_status == 'e'
+        return
+      end
     end
-    p "You have won the game!"
-    p "Would you like to play again? Enter 'yes' or 'no'"
-    print "> "
-    again = $stdin.gets.chomp
   end
 
 end
