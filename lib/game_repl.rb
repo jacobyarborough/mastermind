51bee75a25087f18fb4dc25dc3be23e314e0cec2
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-04 17:21:55 -0600

    Edit #game_won?

--- lib/game_repl.rb
+++ lib/game_repl.rb
@@ -7,7 +7,8 @@ class GameRepl
   end
 
   def game_won?
-    @phrase.num_correct == @phrase.secret_phrase.uniq.length && @phrase.pos_correct == 4
+    @phrase.num_correct == @phrase.secret_phrase.uniq.length
+    && @phrase.pos_correct == @phrase.secret_phrase.length
   end
 
   def assign_guess
@@ -21,10 +22,19 @@ class GameRepl
     guess_count = 0
     until game_won? == true
       assign_guess
-      p "#{phrase.guess_phrase} has #{phrase.num_correct} elements correct with #{phrase.pos_correct} in the correct positions"
-      guess_count += 1
-      p "you've taken #{guess_count} guesses"
+      if phrase.guess_phrase == ['q']
+        return
+      elsif phrase.guess_phrase == ["i"]
+        p "here is some info"
+      elsif phrase.guess_phrase == ["c"]
+        p "The secret phrase is #{phrase.secret_phrase}"
+      else
+        p "#{phrase.guess_phrase} has #{phrase.num_correct} elements correct with #{phrase.pos_correct} in the correct positions"
+          guess_count += 1
+        p "you've taken #{guess_count} guesses"
+      end
     end
+    p "You have won the game!"
   end
 
 end
