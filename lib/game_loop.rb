ec7ad912f9c9a534b9195140ff4e6dd48ccc6fd8
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-05 12:26:15 -0600

    Refactor #go

--- lib/game_loop.rb
+++ lib/game_loop.rb
@@ -8,23 +8,28 @@ class GameLoop
   end
 
   def go
-    #game_status = ''
-    @game_loop_message.intro
-    @game_repl.phrase.guess_phrase.push($stdin.gets.chomp)
     while @game_repl.phrase.guess_phrase != ['q']
-      if @game_repl.phrase.guess_phrase == ['p']
-        @game_loop_message.game_start
-        @game_repl = GameRepl.new
-        @game_repl.play
-        @game_repl.phrase.guess_phrase = []
-        @game_repl.phrase.guess_phrase.push($stdin.gets.chomp)
-      elsif @game_repl.phrase.guess_phrase == ['i']
-        p "here is some info"
-        @game_repl.phrase.guess_phrase = []
-        @game_loop_message.intro
-        @game_repl.phrase.guess_phrase.push($stdin.gets.chomp)
-      elsif @game_repl.phrase.guess_phrase == ['q']
-        return
+      @game_loop_message.intro
+      @game_repl.phrase.guess_phrase.push($stdin.gets.chomp)
+      while @game_repl.phrase.guess_phrase != ['q']
+        if @game_repl.phrase.guess_phrase == ['p']
+          @game_loop_message.game_start
+          @game_repl = GameRepl.new
+          @game_repl.play
+          if @game_repl.phrase.guess_phrase == ['q']
+            @game_repl.phrase.guess_phrase = []
+            break
+          end
+          @game_repl.phrase.guess_phrase = []
+          @game_repl.phrase.guess_phrase.push($stdin.gets.chomp)
+        elsif @game_repl.phrase.guess_phrase == ['i']
+          @game_loop_message.instructions
+          @game_repl.phrase.guess_phrase = []
+          @game_loop_message.intro
+          @game_repl.phrase.guess_phrase.push($stdin.gets.chomp)
+        elsif @game_repl.phrase.guess_phrase == ['q']
+          return
+        end
       end
     end
   end
