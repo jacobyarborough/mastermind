b350b5aa0ece8dc140efc8523e9c1e37c24a2ce7
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-05 09:02:58 -0600

    Refactor #play

--- lib/game_repl.rb
+++ lib/game_repl.rb
@@ -4,39 +4,48 @@ class GameRepl
 
   def initialize
     @phrase = Phrase.new
+    @message = Messages.new
+    @guess_counter = 0
+    @time_diff = 0.0
   end
 
   def game_won?
-    @phrase.num_correct == @phrase.secret_phrase.uniq.length
-    && @phrase.pos_correct == @phrase.secret_phrase.length
+    @phrase.num_correct == @phrase.secret_phrase.uniq.length && @phrase.pos_correct == @phrase.secret_phrase.length
   end
 
   def assign_guess
     p "enter guess"
     print "> "
     guess = $stdin.gets.chomp
-    phrase.parse_guess(guess)
+    @phrase.parse_guess(guess)
   end
 
   def evaluate_guess
-    if phrase.guess_phrase == ['q']
+    if @phrase.guess_phrase == ['q']
       return
-    elsif phrase.guess_phrase == ["i"]
-      message.instructions
-    elsif phrase.guess_phrase == ["c"]
-      message.cheat
+    elsif @phrase.guess_phrase == ["i"]
+      @message.instructions
+    elsif @phrase.guess_phrase == ["c"]
+      @message.cheat(@phrase.secret_phrase)
     else
-      message.default_guess
+      @message.default_guess(@phrase.guess_phrase , @phrase.num_correct, @phrase.pos_correct, @guess_counter)
     end
   end
 
+
   def play
-    guess_count = 0
+    start = Time.now
     until game_won? == true
+      @guess_counter += 1
       assign_guess
       evaluate_guess
+      if @phrase.guess_phrase == ['q']
+        return
+      end 
     end
-    message.game_won
+    finish = Time.now
+    @time_diff = (finish - start)
+    @message.game_won(@phrase.secret_phrase, @guess_counter, @time_diff)
   end
 
 end
