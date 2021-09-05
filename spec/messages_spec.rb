fc972a67d46fb888ccc9a43f0d3af99073ff229d
Jacob Yarborough <jayarborough95@gmail.com>
2021-09-05 13:25:46 -0600

    Test: add #instructions

--- spec/messages_spec.rb
+++ spec/messages_spec.rb
@@ -18,4 +18,19 @@ Would you like to (p)lay, read the (i)nstructions, or (q)uit?
     end
   end
 
+  describe "#instructions" do
+    it "puts the intro message" do
+      expect do
+        Messages.new.instructions
+      end.to output("--------------------------------------------------------------------------------------
+A sequence containing some or all of the elements (r)ed, (g)reen, (b)lue, and (y)ellow
+has been generated. It contains four elements.
+
+Use (q)uit at any time to end the game.
+Use (c)heat to peak at the solution.
+Use (i)nstructions to view the game instructions.
+--------------------------------------------------------------------------------------").to_stdout
+    end
+  end
+
 end
